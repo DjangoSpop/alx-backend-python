771eb9578dd484db0504bd48f3dc4d9f0601f12d
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-04-23 07:36:10 +0200

    Fix duplicate import of random module in basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 import random
 import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """_summary_
     Args:
         max_delay (int, optional): _description_. Defaults to 10.
-
     Returns:
         float: _description_
-    """    
+    """  
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
