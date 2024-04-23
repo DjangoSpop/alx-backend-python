a0979f2558f98c9921904b5dfc6348f0838302d2
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-04-23 08:38:47 +0200

    Add missing comment in wait_random function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,11 +9,12 @@ async def wait_random(max_delay: int = 10) -> float:
 
     Args:
         max_delay (int, optional): The maximum delay in seconds.
-        Defaults to 10.
+            Defaults to 10.
 
     Returns:
         float: The actual delay in seconds.
     """
+    # Generate a random number between 0 and max_delay
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
