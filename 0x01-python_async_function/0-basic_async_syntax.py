bb05cdc2fc980e87ef9faa786c62182ddaaaffb0
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-04-23 07:37:31 +0200

    we can do it

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -10,7 +10,7 @@ async def wait_random(max_delay: int = 10) -> float:
         max_delay (int, optional): _description_. Defaults to 10.
     Returns:
         float: _description_
-    """  
+    """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
