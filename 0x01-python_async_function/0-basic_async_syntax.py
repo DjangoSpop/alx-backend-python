ebbdc660958c4fa796540e22a2c840a663e529d8
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-04-23 07:30:27 +0200

    Fix formatting issues in Python variable annotations

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,9 +2,9 @@
 import random
 import asyncio
 
+
 async def wait_random(max_delay: int = 10) -> float:
     """_summary_
-
     Args:
         max_delay (int, optional): _description_. Defaults to 10.
 
