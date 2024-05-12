52b6ed9f051995471e3cdee7d641feacfea2a1be
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-05-12 18:58:10 +0300

    Refactor utils.py and client.py for better code readability and maintainability

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,45 +1,76 @@
 #!/usr/bin/env python3
-"""module to make unit testing"""
-import unittest
+'''Module to test utils file
+'''
 from parameterized import parameterized
-from utils import access_nested_map
+import unittest
+from utils import (access_nested_map, get_json, memoize)
+from unittest.mock import patch
+
 
 class TestAccessNestedMap(unittest.TestCase):
-    """_summary_
+    '''class for testing access_nestd_map function
+    '''
 
-    Args:
-        unittest (_type_): _description_
-    """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map, path, expected_result):
-        """_summary_
+    def test_access_nested_map(self, nested_map, path, expected):
+        """ Test that the method returns what it is supposed to
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
-        Args:
-            nested_map (_type_): _description_
-            path (_type_): _description_
-            expected_result (_type_): _description_
-        """      
-     
-        self.assertEqual(access_nested_map(nested_map, path), expected_result)
-        with self.assertRaises(KeyError) as cm:
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """ Test that a KeyError is raised for the respective inputs """
+        with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
-        self.assertEqual(str(cm.exception), repr(path[-1]))
-    
+        self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
+
+
+class TestGetJson(unittest.TestCase):
+    """ Class for Testing Get Json """
+
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
-    @mock.patch('requests.get')
-    def test_get_json(self, test_url, test_payload, mock_get):
-        """Test that utils.get_json returns the expected result."""
-        mock_get.return_value.json.return_value = test_payload
+    def test_get_json(self, test_url, test_payload):
+        """ Test for the utils.get_json function to check
+        that it returns the expected result."""
+        config = {'return_value.json.return_value': test_payload}
+        patcher = patch('requests.get', **config)
+        mock = patcher.start()
+        self.assertEqual(get_json(test_url), test_payload)
+        mock.assert_called_once()
+        patcher.stop()
 
-        response = utils.get_json(test_url)
-        mock_get.assert_called_once_with(test_url)
-        self.assertEqual(response, test_payload)
-        
-    
+
+class TestMemoize(unittest.TestCase):
+    """ test class to tes utils.memoize"""
+
+    def test_memoize(self):
+        """ Tests the function when calling a_property twice,
+        the correct result is returned but a_method is only
+        called once using assert_called_once
+        """
+
+        class TestClass:
+            """ Test Class for wrapping with memoize """
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock.assert_called_once()
