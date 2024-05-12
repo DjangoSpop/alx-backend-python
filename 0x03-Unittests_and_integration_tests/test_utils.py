e9594da044682da7f8c5437ba97ed32e63fa7a31
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-05-12 12:59:32 +0300

    Refactor test_utils.py to add a new test case for get_json function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,6 +22,24 @@ class TestAccessNestedMap(unittest.TestCase):
             nested_map (_type_): _description_
             path (_type_): _description_
             expected_result (_type_): _description_
-        """        
+        """      
+     
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(cm.exception), repr(path[-1]))
+    
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @mock.patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Test that utils.get_json returns the expected result."""
+        mock_get.return_value.json.return_value = test_payload
+
+        response = utils.get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(response, test_payload)
+        
     
